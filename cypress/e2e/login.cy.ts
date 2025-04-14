a8f6198ec89a86c622b0561abe2a98dae7fba8c5
tatiana.utrera <tatiana.utrera@5by5.com.br>
2025-04-13 21:42:42 -0300

    inclusão de cenario e que o login nao exista e tente logar na aplicação

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,27 +5,41 @@ import faker from 'faker-br'
 
 describe('Realziar Login', () => {
   let user;
-  beforeEach(() => {
-    user = {
-      email: "teste" + faker.internet.email(),
-      name: faker.name.findName(),
-      password: "1234",
-      confirmationPassword: "1234",
-      withBalance: false
-    }
-    LoginPage.accessRegisterUserPage()
-    RegisterUserPage.registerUser(user)
-
-  })
+  
   describe('Dado que eu possua cadastro no BugBank', () => {
+    beforeEach(() => {
+      user = {
+        email: "teste" + faker.internet.email(),
+        name: faker.name.findName(),
+        password: "1234",
+        confirmationPassword: "1234",
+        withBalance: false
+      }
+      LoginPage.accessRegisterUserPage()
+      RegisterUserPage.registerUser(user)
+    })
     it('CT05 - Quando solicito o login informando dados validos de um usuário que possui cadastro, então o sistema deve acessar o sistema com sucesso', () => {
       LoginPage.login(user)
       HomePage.assertName(user.name)
     })
 
-    it.only('CT06 - Quando solicito o login informando a senha incorreta, então o sistema deve retornar uma mensagem de erro informando que as credencias estão inválidas não permitindo seguir com o fluxo', () => {
+    it('CT06 - Quando solicito o login informando a senha incorreta, então o sistema deve retornar uma mensagem de erro informando que as credencias estão inválidas não permitindo seguir com o fluxo', () => {
       user.password = "12345"
-      cy.get('#btnCloseModal').click()
+      LoginPage.login(user)
+      RegisterUserPage.assertMessageAlert("Usuário ou senha inválido. Tente novamente ou verifique suas informações!")
+    })
+  })
+  describe('Dado que eu não possua cadastro no BugBank',()=>{
+    it('CT07 - Quando solicito o login informando um login e senha que não possuam cadastro, então o sistema deve retornar uma mensagem de erro informando que as credencias estão inválidas não permitindo seguir com o fluxo', () => {
+      LoginPage.accessRegisterUserPage()
+
+      user = {
+        email: "teste" + faker.internet.email(),
+        name: faker.name.findName(),
+        password: "1234",
+        confirmationPassword: "1234",
+        withBalance: false
+      }
       LoginPage.login(user)
       RegisterUserPage.assertMessageAlert("Usuário ou senha inválido. Tente novamente ou verifique suas informações!")
     })
